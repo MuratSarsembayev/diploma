dca6ad37bea89470c5bb1db93061db5af1b7ccaa
MuratSarsembayev <52274981+MuratSarsembayev@users.noreply.github.com>
2020-04-08 15:10:04 +0600

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -155,14 +155,12 @@ async def send_show_takers(message: Message, state: FSMContext):
         month = int(data.get("month"))
         send_date = date(year, month, day).isoformat()
         await db.add_new_sender(city_a, city_b, send_date)
-        text = f"""Список тех кто может перевезти посылку в нужную вам дату
-"""
+        text = "Список тех кто может перевезти посылку в нужную вам дату"
         takers = await db.show_takers(city_a, city_b, send_date)
-        res = """"""
+        res = ""
         for i in takers:
             for j in takers[i]:
                 res += " ".join(takers[i][j])
-            res += "\n"
         text += res
         await message.reply(text,
                         reply_markup=keyboard)
@@ -218,14 +216,12 @@ async def send_show_senders(message: Message, state: FSMContext):
         month = int(data.get("month"))
         take_date = date(year, month, day).isoformat()
         await db.add_new_taker(city_a, city_b, take_date)
-        text = f"""Список тех, кто хочет отправить посылку в нужную вам дату
-"""
+        text = "Список тех, кто хочет отправить посылку в нужную вам дату"
         senders = await db.show_senders(city_a, city_b, take_date)
-        res = """"""
+        res = ""
         for i in senders:
             for j in senders[i]:
                 res += " ".join(senders[i][j])
-            res += "\n"
         text += res
         await message.reply(text,
                         reply_markup=keyboard)
